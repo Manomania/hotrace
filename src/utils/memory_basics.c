e0c5308bc7cb605ae8161ecb0fed6ddf21abb85d
maximart <maxime.martinez96@hotmail.fr>
2025-02-23 13:27:14 +0100

    Refactor(change GNL): Change the GNL by maximart_gnl

--- src/utils/memory_basics.c
+++ src/utils/memory_basics.c
@@ -64,15 +64,15 @@ void	*ft_calloc(size_t el_count, size_t el_size)
 	return (temp);
 }
 
-void	ft_bzero(void *s, size_t n)
-{
-	unsigned char	*o;
-
-	o = (unsigned char *) s;
-	while (n)
-	{
-		*o = 0;
-		o++;
-		n--;
-	}
-}
+// void	ft_bzero(void *s, size_t n)
+// {
+// 	unsigned char	*o;
+//
+// 	o = (unsigned char *) s;
+// 	while (n)
+// 	{
+// 		*o = 0;
+// 		o++;
+// 		n--;
+// 	}
+// }
