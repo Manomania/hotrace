d47a33530cd688c6f69b9c23169bd6f11908dd0a
furia25 <val@ValPc.>
2025-02-23 18:34:37 +0100

    Murmur3 ?

--- src/hashmap/hashmap_managing.c
+++ src/hashmap/hashmap_managing.c
@@ -6,7 +6,7 @@
 /*   By: val <val@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/22 19:12:44 by vdurand           #+#    #+#             */
-/*   Updated: 2025/02/23 18:31:57 by val              ###   ########.fr       */
+/*   Updated: 2025/02/23 18:34:28 by val              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,20 +50,21 @@ void	hashmap_free(t_hashmap *map, void (*del)(void *))
 	free(map);
 }
 
-/*FNV-1a (Fowler-Noll-Vo) Hash*/
+/*Murmur3hash*/
 unsigned long	hash(char *str)
 {
 	unsigned long	hash;
-	int				c;
+	unsigned long	c;
 	int				i;
 
 	hash = 0x811c9dc5;
 	i = 0;
 	while (str[i] != '\0')
 	{
-		c = str[i];
+		c = (unsigned long)str[i];
 		hash ^= c;
-		hash *= 0x01000193;
+		hash *= 0x5bd1e995;
+		hash ^= hash >> 15;
 		i++;
 	}
 	return (hash);
